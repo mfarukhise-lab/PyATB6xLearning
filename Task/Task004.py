822ff2687a49982bff72f673af399c3e0127f458
Muddasar Farukhi <mfarukhi.se@gmail.com>
2025-10-16 00:01:11 +0530

    Push on GIT

---
+++ Task/Task004.py
@@ -0,0 +1,10 @@
+# 1/2*base*height
+#0.5*base*height
+#b=10
+#h=5
+
+b=float (input("Enter base of triangle: "))
+h=float (input("Enter height of triangle: "))
+
+area =0.5*b*h
+print("area of the triangle", area)
---
+++ Task/Task05.py
@@ -0,0 +1,7 @@
+Enter_number = float(input("Enter your number: "))
+
+#if Enter_number >= 100:
+ #   print ("Yes Number is greater than 100")
+#else:
+ #   print ("No it's not greater than 100")
+print("Yes Number is greater than 100" if Enter_number >= 100 else "No it's not greater than 100")
